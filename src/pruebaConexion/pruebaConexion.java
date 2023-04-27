ecbb1d59994efd22f32c4e6f87aa320010f82974
JAT91 <joaquintravia@hotmail.com>
2023-04-27 20:13:23 -0300

    Ok

--- src/pruebaConexion/pruebaConexion.java
+++ src/pruebaConexion/pruebaConexion.java
@@ -19,7 +19,7 @@ public class pruebaConexion {
 		    Statement stmt=con.createStatement();
 		    
 		    //Traigo tabla empleado
-		    ResultSet rs=stmt.executeQuery("SELECT * FROM empleado");
+		    ResultSet rs = stmt.executeQuery("SELECT * FROM empleado");
 		    while(rs.next())
 		    System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5));
 		    
